e00a39f100e1decad3ccad90ae0244d760198f20
João Mesquita <jolitas2005@gmail.com>
2025-10-15 16:22:40 +0100

    Initial commit - EduScrum Awards Spring Boot project

---
+++ src/main/java/com/group7/eduscrums_awards/EduScrumsAwardsApplication.java
@@ -0,0 +1,13 @@
+package com.group7.eduscrums_awards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduScrumsAwardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduScrumsAwardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group7/eduscrums_awards/EduScrumsAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.eduscrums_awards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduScrumsAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
