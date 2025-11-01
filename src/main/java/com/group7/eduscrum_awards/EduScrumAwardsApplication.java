55690b399e201d13e900faad99bdad2ba65aa5da
João Mesquita <jolitas2005@gmail.com>
2025-11-01 18:35:02 +0000

    Initialize EduScrumAwardsApplication with Spring Boot setup

---
+++ src/main/java/com/group7/eduscrum_awards/EduScrumAwardsApplication.java
@@ -0,0 +1,13 @@
+package com.group7.eduscrum_awards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduScrumAwardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduScrumAwardsApplication.class, args);
+    }
+
+}
