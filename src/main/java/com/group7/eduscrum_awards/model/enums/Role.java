9e7909059fa74b39c427dc94e12b80633e71b2c6
João Mesquita <jolitas2005@gmail.com>
2025-11-01 22:06:11 +0000

    Add Role enum to represent user roles in the system

---
+++ src/main/java/com/group7/eduscrum_awards/model/enums/Role.java
@@ -0,0 +1,11 @@
+package com.group7.eduscrum_awards.model.enums;
+
+/**
+ * Represents the roles a User can have in the system.
+ * Using an Enum ensures type safety and prevents invalid role values.
+ */
+public enum Role {
+    STUDENT,
+    TEACHER,
+    ADMIN
+}
