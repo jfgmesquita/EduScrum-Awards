9cb378c1f840594c12652d13b2d5d39f395541bd
João Mesquita <jolitas2005@gmail.com>
2025-11-04 00:14:59 +0000

    Add CourseDTO to expose basic course information via REST API

---
+++ src/main/java/com/group7/eduscrum_awards/dto/CourseDTO.java
@@ -0,0 +1,31 @@
+package com.group7.eduscrum_awards.dto;
+
+import com.group7.eduscrum_awards.model.Course;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * DTO (Data Transfer Object) for the {@link Course} entity.
+ *
+ * This DTO is used to expose basic course information (id and name)
+ * via the REST API, typically as a response after creation or in lists.
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+public class CourseDTO {
+
+    private Long id;
+    private String name;
+
+    /**
+     * Create a DTO from the given entity.
+     *
+     * @param course the source {@link Course} entity (must not be null)
+     */
+    public CourseDTO(Course course) {
+        this.id = course.getId();
+        this.name = course.getName();
+    }
+}
