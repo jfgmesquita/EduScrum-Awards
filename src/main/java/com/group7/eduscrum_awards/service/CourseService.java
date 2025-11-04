37e12c8e0d80ef39dd9b4633409836711601c56c
João Mesquita <jolitas2005@gmail.com>
2025-11-04 15:05:23 +0000

    Add method to assign existing Teacher to Course in CourseService interface

--- src/main/java/com/group7/eduscrum_awards/service/CourseService.java
+++ src/main/java/com/group7/eduscrum_awards/service/CourseService.java
@@ -2,6 +2,8 @@ package com.group7.eduscrum_awards.service;
 
 import com.group7.eduscrum_awards.dto.CourseCreateDTO;
 import com.group7.eduscrum_awards.dto.CourseDTO;
+import com.group7.eduscrum_awards.exception.DuplicateResourceException;
+import com.group7.eduscrum_awards.exception.ResourceNotFoundException;
 
 /**
  * Service Interface (Contract) for Course operations.
@@ -19,6 +21,17 @@ public interface CourseService {
      * @throws DuplicateResourceException if a Course with the same name already exists within that Degree.
      */
     CourseDTO registerCourseForDegree(Long degreeId, CourseCreateDTO courseCreateDTO);
+
+    /**
+     * Assigns an existing Teacher to an existing Course.
+     * This method is transactional to ensure the relationship is saved.
+     *
+     * @param courseId The ID of the course.
+     * @param teacherId The ID of the teacher to add.
+     * @return A DTO of the updated Course.
+     * @throws ResourceNotFoundException if the course or teacher is not found.
+     */
+    CourseDTO addTeacherToCourse(Long courseId, Long teacherId);
 
     // Future methods can be added here, e.g.:
     // List<CourseDTO> getAllCourses();
