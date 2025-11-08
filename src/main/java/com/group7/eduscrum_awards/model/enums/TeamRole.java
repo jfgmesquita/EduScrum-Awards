ac1d624ac5a2065cbd3a74576f8c711e9fb1b457
João Mesquita <jolitas2005@gmail.com>
2025-11-08 15:44:31 +0000

    Add TeamRole enum to define roles within a Team

---
+++ src/main/java/com/group7/eduscrum_awards/model/enums/TeamRole.java
@@ -0,0 +1,8 @@
+package com.group7.eduscrum_awards.model.enums;
+
+/** Represents the contextual role a Student holds within a specific Team. */
+public enum TeamRole {
+    PRODUCT_OWNER,
+    SCRUM_MASTER,
+    DEVELOPER
+}
