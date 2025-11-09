ddb5b6962f6deb66e695fb94461a480820e90055
João Mesquita <jolitas2005@gmail.com>
2025-11-09 18:23:43 +0000

    Add assignTask endpoint in TaskController for assigning tasks to team members

--- src/main/java/com/group7/eduscrum_awards/controller/TaskController.java
+++ src/main/java/com/group7/eduscrum_awards/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.group7.eduscrum_awards.controller;
 
 import com.group7.eduscrum_awards.dto.TaskCreateDTO;
 import com.group7.eduscrum_awards.dto.TaskDTO;
+import com.group7.eduscrum_awards.dto.TaskAssignDTO;
 import com.group7.eduscrum_awards.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,23 @@ public class TaskController {
         TaskDTO newTask = taskService.createTask(sprintId, taskCreateDTO);
         // Return the new task and a 201 Created status
         return new ResponseEntity<>(newTask, HttpStatus.CREATED);
+    }
+
+    /**
+     * Endpoint to assign a Task to a TeamMember (Developer).
+     * This endpoint is intended to be called by a Student (Product Owner).
+     * Accessible via: PATCH http://localhost:8080/api/v1/tasks/{taskId}/assign
+     *
+     * @param taskId The ID of the Task to be updated.
+     * @param assignDTO The JSON body containing the teamMemberId.
+     * @return A ResponseEntity containing the updated TaskDTO and HTTP status 200.
+     */
+    @PatchMapping("/tasks/{taskId}/assign")
+    public ResponseEntity<TaskDTO> assignTask(@PathVariable Long taskId,
+            @Valid @RequestBody TaskAssignDTO assignDTO) {
+        
+        TaskDTO updatedTask = taskService.assignTask(taskId, assignDTO);
+        // Return 200 OK with the updated task
+        return new ResponseEntity<>(updatedTask, HttpStatus.OK);
     }
 }
