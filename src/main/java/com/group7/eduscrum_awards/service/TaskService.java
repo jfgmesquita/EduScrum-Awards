592ad9ab7be676a12795cc6d4d6b02b71380b128
João Mesquita <jolitas2005@gmail.com>
2025-11-09 18:23:33 +0000

    Add assignTask method to TaskService for assigning tasks to team members

--- src/main/java/com/group7/eduscrum_awards/service/TaskService.java
+++ src/main/java/com/group7/eduscrum_awards/service/TaskService.java
@@ -1,7 +1,9 @@
 package com.group7.eduscrum_awards.service;
 
+import com.group7.eduscrum_awards.dto.TaskAssignDTO;
 import com.group7.eduscrum_awards.dto.TaskCreateDTO;
 import com.group7.eduscrum_awards.dto.TaskDTO;
+import com.group7.eduscrum_awards.exception.ResourceNotFoundException;
 
 /** Service Interface (Contract) for Task operations. */
 public interface TaskService {
@@ -18,4 +20,18 @@ public interface TaskService {
      * @throws AccessDeniedException if the user is not the PO for this project.
      */
     TaskDTO createTask(Long sprintId, TaskCreateDTO taskCreateDTO);
+
+    /**
+     * Assigns an existing Task to a specific TeamMember (Developer).
+     * This operation validates that the logged-in user is the
+     * Product Owner for the project this task belongs to.
+     *
+     * @param taskId The ID of the Task to be updated.
+     * @param assignDTO The DTO containing the ID of the TeamMember to assign.
+     * @return The DTO of the updated Task.
+     * @throws ResourceNotFoundException if the taskId or teamMemberId does not exist.
+     * @throws AccessDeniedException if the user is not the PO for this project.
+     * @throws IllegalArgumentException if the developer is not on a team for this project.
+     */
+    TaskDTO assignTask(Long taskId, TaskAssignDTO assignDTO);
 }
