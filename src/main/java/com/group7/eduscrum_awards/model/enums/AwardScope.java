adf9105ba29be3db991e0e4eb4bdbb7bd20427d5
João Mesquita <jolitas2005@gmail.com>
2025-12-08 22:51:54 +0000

    Add AwardScope enum to define award recipients

---
+++ src/main/java/com/group7/eduscrum_awards/model/enums/AwardScope.java
@@ -0,0 +1,7 @@
+package com.group7.eduscrum_awards.model.enums;
+
+/** Defines who receives the award. */
+public enum AwardScope {
+    STUDENT, // Individual award
+    TEAM     // Team award (but assigned to members individually)
+}
