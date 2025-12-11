8c40f31bd38ba8a198d5d2d74474a5b17dc53006
João Mesquita <jolitas2005@gmail.com>
2025-12-11 00:43:14 +0000

    Add CourseStatsDTO for course-specific statistics tracking

---
+++ src/main/java/com/group7/eduscrum_awards/dto/stats/CourseStatsDTO.java
@@ -0,0 +1,14 @@
+package com.group7.eduscrum_awards.dto.stats;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/** DTO for course-specific statistics. */
+@AllArgsConstructor
+@Getter
+@Setter
+public class CourseStatsDTO {
+    private long studentsCount;
+    private long teachersCount;
+}
