6f64a9ea314d28a512e53772b6d5705634325d4e
João Mesquita <jolitas2005@gmail.com>
2025-12-12 02:46:34 +0000

    Enhance StudentUpdateDTO with validation annotations for name and email fields

--- src/main/java/com/group7/eduscrum_awards/dto/StudentUpdateDTO.java
+++ src/main/java/com/group7/eduscrum_awards/dto/StudentUpdateDTO.java
@@ -1,4 +1,9 @@
 package com.group7.eduscrum_awards.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -6,7 +11,15 @@ import lombok.Setter;
 @Getter 
 @Setter
 public class StudentUpdateDTO {
+
+    @NotBlank(message = "Name cannot be blank.")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters.")
     private String name;
+
+    @NotBlank(message = "Email cannot be blank.")
+    @Email(message = "Email should be valid.")
+    @Size(max = 100, message = "Email must be less than 100 characters.")
     private String email;
+
     private Long degreeId; // Optional: to update the associated degree
 }
