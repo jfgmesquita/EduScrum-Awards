a13ae0fbd81a69f792f0726dd12096cf5c9c9b9f
João Mesquita <jolitas2005@gmail.com>
2025-12-12 02:21:33 +0000

    Add CourseUpdateDTO for updating course information

---
+++ src/main/java/com/group7/eduscrum_awards/dto/CourseUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.group7.eduscrum_awards.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+/** Data Transfer Object for updating Course information. */
+@Getter
+@Setter
+public class CourseUpdateDTO {
+    private String name;
+}
