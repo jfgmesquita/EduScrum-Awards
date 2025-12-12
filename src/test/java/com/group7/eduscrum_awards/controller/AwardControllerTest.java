8904aace656ff1951cfd7434b0ead8b6e3520981
João Mesquita <jolitas2005@gmail.com>
2025-12-12 18:04:39 +0000

    Add documentation comment for AwardControllerTest class

--- src/test/java/com/group7/eduscrum_awards/controller/AwardControllerTest.java
+++ src/test/java/com/group7/eduscrum_awards/controller/AwardControllerTest.java
@@ -30,6 +30,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/** Unit tests for {@link AwardController}. */
 @WebMvcTest(AwardController.class)
 class AwardControllerTest {
 
