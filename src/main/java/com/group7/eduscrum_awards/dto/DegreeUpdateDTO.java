b6c6b0a5bf5dd6c91e1ff86f082bcbcfec0b7f0d
João Mesquita <jolitas2005@gmail.com>
2025-12-12 02:21:35 +0000

    Add DegreeUpdateDTO for updating degree information

---
+++ src/main/java/com/group7/eduscrum_awards/dto/DegreeUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.group7.eduscrum_awards.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+/** Data Transfer Object for updating Degree information. */
+@Getter
+@Setter
+public class DegreeUpdateDTO {
+    private String name;
+}
